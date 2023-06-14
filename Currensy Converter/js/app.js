cc2a60ef96169295c3a86d39dd647b08c220db06
bekcoder77 <127332753+bekcoder77@users.noreply.github.com>
2023-06-14 06:17:38 +0500

    Add files via upload

---
+++ Currensy Converter/js/app.js
@@ -0,0 +1,69 @@
+const dropList = document.querySelectorAll("form select"),
+  fCurrensy = document.querySelector(".from select"),
+  toCurrency = document.querySelector(".to select"),
+  getButton = document.querySelector("form button");
+  for (let i = 0; i < dropList.length; i++) {
+  for (let currency_code in country_list) {
+    let selected ;
+    if (i == 0) {
+        selected = currency_code == "USD" ? "selected" : "";
+      } else if (i == 1) {
+        selected = currency_code == "UZS" ? "selected" : "";
+      }
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
+  dropList[i].addEventListener("change", (e) => {
+    loadFlag(e.target);
+  });
+}
+function loadFlag(element) {
+  for (let code in country_list) {
+    if (code == element.value) {
+      let imgTag = element.parentElement.querySelector("img");
+      imgTag.src = `https://flagcdn.com/48x36/${country_list[
+        code
+      ].toLowerCase()}.png`;
+    }
+  }
+}
+window.addEventListener("load", () => {
+  getExchangeRate();
+});
+
+getButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector(".icon");
+exchangeIcon.addEventListener("click", () => {
+  let tempCode = fCurrensy.value;
+  fCurrensy.value = toCurrency.value;
+  toCurrency.value = tempCode;
+  loadFlag(fCurrensy);
+  loadFlag(toCurrency);
+  getExchangeRate();
+});
+
+function getExchangeRate() {
+  const amount = document.querySelector(".amount input");
+  const exchangeRateTxt = document.querySelector("form .exchange-rate");
+  let amountVal = amount.value;
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
+  exchangeRateTxt.innerText = "Getting exchange rate...";
+  let url = `https://v6.exchangerate-api.com/v6/33efc4ec6d4b48d329c401c3/latest/${fCurrensy.value}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let exchangeRate = result.conversion_rates[toCurrency.value];
+      let totalExRate = (amountVal * exchangeRate).toFixed(2);
+      exchangeRateTxt.innerHTML = `${amountVal} ${fCurrensy.value} = ${totalExRate} ${toCurrency.value}`;
+    })
+    .catch(() => {
+      exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
